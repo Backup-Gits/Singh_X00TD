bd0440ac64453a5dc4c6a68a3e1d116909f83cf8
mpschahal16 <mpschahal16@gmail.com>
2018-10-29 01:01:46 +0530

    Revert "cpufreq.c:hardset to 633hz"
    This reverts commit dec75d9ea3d70cd5d7bc1cb9d0772f8babb0c745.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2260,8 +2260,7 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 
 	scale_freq_capacity(new_policy, NULL);
 
-	/*policy->min = new_policy->min;*/
-	policy->min = 633600;
+	policy->min = new_policy->min;
 	policy->max = new_policy->max;
 	trace_cpu_frequency_limits(policy->max, policy->min, policy->cpu);
 
